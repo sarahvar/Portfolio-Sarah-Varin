df03a686e2b7fe3e0743c78ac92cc01aec359df0
sarahvar <sarahvarin95@gmail.com>
2023-04-09 00:04:04 +0100

    error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,15 @@
+import { NavLink } from "react-router-dom"
+
 export default function Error404 () {
     return (
-        <div>Je suis une erreur</div>
+     <div className="error404">
+        <div className="error404Content">
+            <h3>Désolée cette page n'existe pas !</h3>
+            <NavLink exact to="/">
+                <i className="fa-solid fa-house"></i>
+                <span> Accueil</span>
+            </NavLink>
+        </div>
+     </div>
     )
 }
