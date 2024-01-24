d017aa26c149a2cf6812b341236ed8e223c0e386
sarahvar <sarahvarin95@gmail.com>
2024-01-24 19:18:55 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,21 @@ const babelConfig = {
 
 export default defineConfig({
   plugins: [react(babelConfig)],
+  server: {
+    fs: {
+      strict: false,
+    },
+    middleware: (req, res, next) => {
+      if (req.originalUrl.endsWith('.jsx')) {
+        res.type('application/javascript');
+      }
+      next();
+    },
+  },
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+      external: ['react-router-dom'], // Ajoutez ici d'autres modules externes si nécessaire
     },
   },
 });
+
