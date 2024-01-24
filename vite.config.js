e33d303b42d3556aa756557d9c240b2b4dd20ed8
sarahvar <sarahvarin95@gmail.com>
2024-01-24 16:59:20 +0100

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    middleware: {
-      // Middleware pour définir le type MIME des fichiers .js
-      '*.@(js)': (req, res, next) => {
+    fs: {
+      // permet d'éviter les erreurs liées à l'extension par défaut ".jsx"
+      strict: false,
+    },
+    middleware: (req, res, next) => {
+      // Change le type MIME uniquement pour les fichiers .js et .jsx
+      if (req.originalUrl.endsWith('.js') || req.originalUrl.endsWith('.jsx')) {
         res.setHeader('Content-Type', 'application/javascript');
-        next();
-      },
+      }
+      next();
     },
   },
 });
