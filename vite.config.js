638a6d6a192cd0b13e97fee27862ecc751103799
sarahvar <sarahvarin95@gmail.com>
2024-01-24 18:09:47 +0100

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuration Babel pour le support JSX
 const babelConfig = {
   presets: ['@babel/preset-react'],
 };
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       ...babelConfig,
     }),
   ],
-  server: {
-    fs: {
-      // permet d'éviter les erreurs liées à l'extension par défaut ".jsx"
-      strict: false,
-    },
-    middleware: (req, res, next) => {
-      // Change le type MIME uniquement pour les fichiers .js et .jsx
-      if (req.originalUrl.endsWith('.js') || req.originalUrl.endsWith('.jsx')) {
-        res.setHeader('Content-Type', 'application/javascript');
-      }
-      next();
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
     },
   },
 });
-
