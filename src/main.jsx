c676bf78f51445b88db18203e4e88e02c9f23cd0
sarahvar <sarahvarin95@gmail.com>
2024-01-24 15:51:21 +0100

    js

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./styles/styles.scss";
-import Home from "./pages/Home.jsx";
-import Contact from "./pages/Contact.jsx";
-import Error404 from "./pages/Error404.jsx";
-import Competences from "./pages/Competences.jsx";
-import Portfolio from "./pages/Portfolio.jsx";
+import Home from "./pages/Home.js";
+import Contact from "./pages/Contact.js";
+import Error404 from "./pages/Error404.js";
+import Competences from "./pages/Competences.js";
+import Portfolio from "./pages/Portfolio.js";
 
 const router = createBrowserRouter([
   {
