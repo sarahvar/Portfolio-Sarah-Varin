3dd04c3c20ad33cee23ad930bf2d95e7f1d20730
sarahvar <sarahvarin95@gmail.com>
2024-01-25 14:12:33 +0100

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const babelConfig = {
-  presets: ['@babel/preset-react'],
-};
 
 export default defineConfig({
   plugins: [react(babelConfig)],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-      output: {
-        manualChunks: {
-          'react-router-dom': ['react-router-dom'],
-        },
-      },
-    },
-  },
   server: {
     fs: {
       strict: false,
     },
     middleware: (req, res, next) => {
       if (req.originalUrl.endsWith('.jsx')) {
-        res.type('application/javascript; charset=utf-8'); // Ajout de "; charset=utf-8"
+        res.type('application/javascript');
       }
       next();
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/sarahvar/Portfolio-Sarah-Varin/' : '/',
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Ajoutez ici d'autres modules externes si nécessaire
+    },
+  },
 });
