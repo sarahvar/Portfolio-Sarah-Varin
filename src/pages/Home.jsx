75e4bbc06d20c33a17f3c33d78f9f82b60732365
sarahvar <sarahvarin95@gmail.com>
2024-01-25 22:31:25 +0100

    cv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
               href="./media/Cv_Sarah_Varin_Developpeuse-Web.pdf"
               target="blank"
             >
-              Télécharger CV
+              Télécharger<br> CV </br>
             </a>
             <figure>
               <h3>Présentation Sarah Varin</h3>
