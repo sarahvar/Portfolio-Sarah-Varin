304c232d74e73b2c01acbc8b7094c4231fd45b5d
sarahvar <sarahvarin95@gmail.com>
2024-01-25 14:17:29 +0100

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(babelConfig)],
-  server: {
-    fs: {
-      strict: false,
-    },
-    middleware: (req, res, next) => {
-      if (req.originalUrl.endsWith('.jsx')) {
-        res.type('application/javascript');
-      }
-      next();
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Ajoutez ici d'autres modules externes si nécessaire
-    },
-  },
-});
+  plugins: [react()],
+})
