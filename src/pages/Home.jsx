492cb94116fcae7760851b2c1782dbac8817baf5
sarahvar <sarahvarin95@gmail.com>
2024-05-14 23:11:57 +0200

    lettre_de_recommandation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,23 @@ export default function Home() {
       <Navigation />
       <div className="homeContent">
         <div className="content">
-        <h1>Sarah <span class="varin">Varin</span></h1>
+          <h1>Sarah <span className="varin">Varin</span></h1>
           <h2>Développeuse Full Stack</h2>
           <div className="pdf">
             <a
-              href="./media/CV_Sarah-Varin_developpeuse-Full Stack.pdf"
-              target="blank"
+              href="./media/CV_Sarah-Varin_developpeuse-FullStack.pdf"
+              target="_blank"
             >
-              Télécharger mon CV
+              Télécharger mon CV 
+            </a>
+            <a
+              href="./media/recommandation_de_Celine_Bernardo.pdf"
+              target="_blank"
+            >
+              Télécharger ma lettre de recommandation
             </a>
             <figure>
-           <h4> Diplômée de la formation Developpeuse web, je suis passionée par le monde de l'informatique et la programmation.
-            Je me forme en Java et Angular.
-             Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
+              <h4> Diplômée de la formation Développeuse web, je suis passionnée par le monde de l'informatique et la programmation. Je me forme en Java et Angular. Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
             </figure>
           </div>
         </div>
@@ -26,3 +30,4 @@ export default function Home() {
     </div>
   );
 }
+
