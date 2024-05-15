ea742e7c0869602210922eae27eb8ab29c9acff1
sarahvar <sarahvarin95@gmail.com>
2024-05-16 01:19:10 +0200

    CV-mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,23 @@ export default function Home() {
         <div className="content">
           <h1>Sarah <span className="varin">Varin</span></h1>
           <h2>Développeuse Full Stack</h2>
-            <figure>
-              <h4> Diplômée de la formation Développeuse web, je suis passionnée par le monde de l'informatique et la programmation. Je me forme en Java et Angular. Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
-            </figure>
+          <figure>
+            <h4> Diplômée de la formation Développeuse web, actuellement en formation développeuse d'application JavaScript/React, je suis passionnée par le monde de l'informatique et la programmation. Je me forme en Java et Angular. Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
+          </figure>
           <div className="pdf">
             <a
               href="./media/CV_Sarah-Varin_developpeuse-Full Stack.pdf"
               target="_blank"
+              className="desktop-cv"
             >
-              Télécharger mon CV 
+              Télécharger mon CV
+            </a>
+            <a
+              href="./media/CV_Sarah-Varin_Developpeuse-Full Stack_mobile.pdf"
+              target="_blank"
+              className="mobile-cv"
+            >
+              Télécharger mon CV
             </a>
             <a
               href="./media/recommandation_de_Celine_Bernardo.pdf"
@@ -25,7 +33,7 @@ export default function Home() {
               Télécharger ma lettre de recommandation Girelle Production
             </a>
             <a
-              href="./media/Lettre de recommandation_Omer_Ben-Salah.pdf"
+              href="./media/Lettre_de_recommandation_Omer_Ben-Salah.pdf"
               target="_blank"
             >
               Télécharger ma lettre de recommandation Mentor
