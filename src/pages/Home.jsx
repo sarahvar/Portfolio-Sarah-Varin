7232876c3469bf5e4cb1432a96f40b3a6af8ecbd
sarahvar <sarahvarin95@gmail.com>
2024-05-15 23:44:48 +0200

    CV

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,6 @@
-import React from 'react';
-import { isMobile } from 'react-device-detect';
-import Navigation from '../components/Navigation';
+import Navigation from "../components/Navigation";
 
 export default function Home() {
-  const cvLink = isMobile 
-    ? './media/CV_Sarah-Varin_Developpeuse-Full Stack_mobile' 
-    : './media/CV_Sarah-Varin_developpeuse-Full Stack.pdf';
-
   return (
     <div className="home">
       <Navigation />
@@ -14,17 +8,26 @@ export default function Home() {
         <div className="content">
           <h1>Sarah <span className="varin">Varin</span></h1>
           <h2>Développeuse Full Stack</h2>
-          <figure>
-            <h4> Diplômée de la formation Développeuse web, actuellement en formation développeuse d'application JavaScript/React, je suis passionnée par le monde de l'informatique et la programmation. Je me forme en Java et Angular. Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
-          </figure>
+            <figure>
+              <h4> Diplômée de la formation Développeuse web, je suis passionnée par le monde de l'informatique et la programmation. Je me forme en Java et Angular. Je suis disponible pour une alternance, un CDI ou un CDD à compter d'août 2024.</h4>
+            </figure>
           <div className="pdf">
-            <a href={cvLink} target="_blank">
+            <a
+              href="./media/CV_Sarah-Varin_developpeuse-Full Stack.pdf"
+              target="_blank"
+            >
               Télécharger mon CV 
             </a>
-            <a href="./media/recommandation_de_Celine_Bernardo.pdf" target="_blank">
+            <a
+              href="./media/recommandation_de_Celine_Bernardo.pdf"
+              target="_blank"
+            >
               Télécharger ma lettre de recommandation Girelle Production
             </a>
-            <a href="./media/Lettre de recommandation_Omer_Ben-Salah.pdf" target="_blank">
+            <a
+              href="./media/Lettre de recommandation_Omer_Ben-Salah.pdf"
+              target="_blank"
+            >
               Télécharger ma lettre de recommandation Mentor
             </a>
           </div>
