562859328b59f92e3bfd3d2dd40a5e2e0fce1589
sarahvar <sarahvarin95@gmail.com>
2024-05-20 22:35:38 +0200

    size

--- src/components/competences/Experiences.jsx
+++ src/components/competences/Experiences.jsx
@@ -5,14 +5,14 @@ export default function Experiences() {
       <div className="exp-1">
         <h4>Développement web</h4>
         <h5>02-05-2022 au 31-05-2022</h5>
-        <p>
+        <p className="paragraph-margin">
           J'ai mis en pratique HTML, CSS et PHP. J'ai créer une stratégie de contenu web et j'étais au contact des clients pour répondre aux questions et gérer les retours.
         </p>
       </div>
       <div className="exp-2">
         <h4>Développement web</h4>
         <h5>01-06-2022 au 01-07-2022</h5>
-        <p>
+        <p className="paragraph-margin">
           J'ai développer une API en mettant en oeuvre des opérations CRUD.
           Ecrire des tests unitaires et faire du maquettage pour un client.
         </p>
