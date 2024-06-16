3ae8c4e5dc96f7317eb3768129e3288fbddc4f33
sarahvar <sarahvarin95@gmail.com>
2024-06-16 16:45:35 +0200

    Informatique

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="homeContent">
         <div className="content">
           <h1>Sarah <span className="varin">Varin</span></h1>
-          <h2>Développeuse Full Stack</h2>
+          <h2>Développeuse Informatique</h2>
           <figure>
             <h4> Je suis passionnée par le développement et actuellement en formation développeuse d'application JavaScript/React. À partir d'août 2024, je serai disponible pour un contrat d'apprentissage via OpenClassrooms. Cette formation, d'une durée de deux ans, se déroulera à raison de quatre jours en entreprise et me permettra de me spécialiser en Java ou C# ASP.NET, niveau Bac +3.</h4>
           </figure>
