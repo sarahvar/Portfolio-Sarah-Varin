98df00666a3f8518cb3e04342f7643bc6a4c86ce
sarahvar <sarahvarin95@gmail.com>
2024-09-09 22:09:52 +0200

    intuler+cv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,20 +9,20 @@ export default function Home() {
           <h1>
             Sarah <span className="varin">Varin</span>
           </h1>
-          <h2>Développeuse Informatique</h2>
+          <h2>Développeuse web</h2>
           <figure>
             <figcaption>
               <h4>
                 Passionnée par le développement, je suis ravie d'annoncer que j'ai terminé ma formation en développement d'application JavaScript/React le 13 août 2024, qui a duré un an et deux mois.
                 <p>
-                  Cette formation m'a apporté beaucoup de connaissances et de pratiques. Je suis disponible pour un contrat d'apprentissage. Cette formation, d'une durée de deux ans, se déroulera à raison de quatre jours en entreprise.
+                  Cette formation m'a apporté beaucoup de connaissances et de pratiques. Je suis disponible pour un contrat d'apprentissage dans le développement web. Cette formation, d'une durée de deux ans, se déroulera à raison de quatre jours en entreprise et un jour en école.
                 </p>
               </h4>
             </figcaption>
           </figure>
           <div className="pdf">
             <a
-              href="./media/CV_Sarah-Varin_developpeuse-informatique.pdf"
+              href="./media/CV_Sarah-Varin_developpeuse-web.pdf"
               target="_blank"
               className="desktop-cv"
             >
