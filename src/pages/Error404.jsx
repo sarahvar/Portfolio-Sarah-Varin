8861b977465ddef7e016b0b0af029b7476f532a6
sarahvar <sarahvarin95@gmail.com>
2024-10-07 23:17:45 +0200

    page-erreur

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,15 +1,19 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
-export default function Error404 () {
-    return (
-     <div className="error404">
-        <div className="error404Content">
-            <h3>Désolée cette page n'existe pas !</h3>
-            <NavLink exact to="/">
-                <i className="fa-solid fa-house"></i>
-                <span> Accueil</span>
-            </NavLink>
-        </div>
-     </div>
-    )
+export default function Error404() {
+  return (
+    <div className="error404">
+      {/* Ajout du GIF du phare */}
+      <div className="phare-container">
+        <img src="/public/media/phare.gif" alt="Phare" className="phare-gif" />
+      </div>
+      <div className="error404Content">
+        <h3>Désolée cette page n'existe pas !</h3>
+        <NavLink exact="true" to="/">
+          <i className="fa-solid fa-house"></i>
+          <span> Accueil</span>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
