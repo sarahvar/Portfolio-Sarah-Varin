008a2652d28f6586d26e5557860245fcf7d86f95
sarahvar <sarahvarin95@gmail.com>
2025-01-25 21:40:41 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
   define: {
     APP_ENV: process.env.VITE_VERCEL_ENV,
